6caa084ae9ae902623ef2617329bb6b5a82a387b
Nichlas <nichlas.flindt.krabbe@gmail.com>
2025-05-19 08:17:29 +0200

    .

---
+++ fruit-admin/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <LandingPage />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', sans-serif;
+  line-height: 1.6;
+  color: #333;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
